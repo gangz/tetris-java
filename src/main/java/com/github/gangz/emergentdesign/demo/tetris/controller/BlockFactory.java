759ac1465f223353a16e568f79fe125a91c7968c
Gang ZHANG <gangz2009@gmail.com>
2022-07-12 13:47:32 +0800

    make block factory select shape randomly

--- src/main/java/com/github/gangz/emergentdesign/demo/tetris/controller/BlockFactory.java
+++ src/main/java/com/github/gangz/emergentdesign/demo/tetris/controller/BlockFactory.java
@@ -1,15 +1,25 @@
 package com.github.gangz.emergentdesign.demo.tetris.controller;
 
+import com.github.gangz.emergentdesign.demo.tetris.shape.Shape;
 import com.github.gangz.emergentdesign.demo.tetris.shape.ShapeI;
 import com.github.gangz.emergentdesign.demo.tetris.shape.ShapeZ;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockFactory {
+    List<Shape> shapes = new ArrayList<>();
+    public BlockFactory(){
+        shapes.add(new ShapeI());
+        shapes.add(new ShapeZ());
+    }
     public Block makeEmptyBlock() {
         return  new Block();
     }
 
     public Block makeRandomBlock() {
-        return makeBlockI();
+        int index = (int) (Math.random()*shapes.size());
+        return new Block(shapes.get(index));
     }
 
 
