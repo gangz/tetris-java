a9c758d1ddad3ecac51ac04b8bf230487c965fe4
gangz <gangz@fudan.edu.cn>
2022-08-06 11:07:02 +0800

    make game can be run automatically

--- src/main/java/com/github/gangz/emergentdesign/demo/tetris/genetic/AutoGame.java
+++ src/main/java/com/github/gangz/emergentdesign/demo/tetris/genetic/AutoGame.java
@@ -2,36 +2,50 @@ package com.github.gangz.emergentdesign.demo.tetris.genetic;
 
 import com.github.gangz.emergentdesign.demo.tetris.ai.AIPlayer;
 import com.github.gangz.emergentdesign.demo.tetris.ai.Parameter;
+import com.github.gangz.emergentdesign.demo.tetris.controller.BlockCreatedEvent;
 import com.github.gangz.emergentdesign.demo.tetris.controller.Game;
 import com.github.gangz.emergentdesign.demo.tetris.ui.GameUI;
 
-public class Tuner implements GameUI {
-    Game game = null;
-    public static void main(String[] args)
-    {
-        new Tuner().play();
-    }
+import java.util.Observable;
+import java.util.Observer;
 
+public class AutoGame implements GameUI, Observer {
+    Game game = null;
+    Integer score = 0;
+    Integer blocks = 500;
+    public static void main(String[] args){
+        AutoGame game = new AutoGame();
+        game.play();
+        System.out.println(game.score);
+    }
     @Override
     public void setController(Game game) {
-        this.game = game;
     }
     @Override
     public void dataChanged() {
         if (game!=null)
-            System.out.println(game.getScore());
+            this.score = game.getScore();
     }
 
     @Override
     public void notifyGameOver() {
-        play();
+        score = game.getScore();
+        this.blocks=-1;
     }
+
     public void play(){
         game = new Game(this);
+        game.addObserver(this);
         AIPlayer player = new AIPlayer(new Parameter(),game);
         player.start();
-        while(true){
+        while(this.blocks>0){
             game.moveDown();
         }
+    }
+
+    @Override
+    public void update(Observable o, Object event) {
+        if (event instanceof BlockCreatedEvent)
+            this.blocks--;
     }
 }
